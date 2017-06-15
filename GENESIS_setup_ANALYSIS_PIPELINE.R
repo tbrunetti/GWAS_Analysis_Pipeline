a53b136b8d64a47f9ac7c6d36d95c7b6c7da5bf3
tbunetti <tonya.brunetti@colorado.edu>
2017-06-15 14:58:34 -0600

    changed script to add PCs 1-20

--- GENESIS_setup_ANALYSIS_PIPELINE.R
+++ GENESIS_setup_ANALYSIS_PIPELINE.R
@@ -25,14 +25,18 @@ iids <- getScanID(genoData)
 ###Run PC analysis
 Kingmat <- king2mat(file.kin0=file.kin0,file.kin=NULL,type="kinship",iids = iids)
 mypcair <- pcair(genoData = genoData, kinMat = Kingmat,divMat = Kingmat)
-mypcrel <- pcrelate(genoData = genoData, pcMat = mypcair$vectors[,1:8],training.set = mypcair$unrels)
+mypcrel <- pcrelate(genoData = genoData, pcMat = mypcair$vectors[,1:20],training.set = mypcair$unrels)
 
 pheno <- as.vector(as.matrix(read.table(phenoFile,header=F,na.string="NA")['V2']))
 pheno <- pheno - 1
 
 scanAnnot <- ScanAnnotationDataFrame(data.frame(scanID = mypcrel$sample.id,pc1 = mypcair$vectors[,1],pc2 = mypcair$vectors[,2],
 	pc3 = mypcair$vectors[,3],pc4 = mypcair$vectors[,4],pc5 = mypcair$vectors[,5],pc6 = mypcair$vectors[,6],
-	pc7 = mypcair$vectors[,7],pc8 = mypcair$vectors[,8], pheno = pheno))
+	pc7 = mypcair$vectors[,7],pc8 = mypcair$vectors[,8], pc9 = mypcair$vectors[,9],
+	pc10 = mypcair$vectors[,10],pc11 = mypcair$vectors[,11],pc12 = mypcair$vectors[,12],
+	pc13 = mypcair$vectors[,13],pc14 = mypcair$vectors[,14],pc15 = mypcair$vectors[,15],
+	pc16 = mypcair$vectors[,16],pc17 = mypcair$vectors[,17],pc18 = mypcair$vectors[,18],
+	pc19 = mypcair$vectors[,19],pc20 = mypcair$vectors[,20],pheno = pheno))
 covMatList <- list("Kin" = pcrelateMakeGRM(mypcrel))
 
 # creates a binary file -- can open in R with load()
