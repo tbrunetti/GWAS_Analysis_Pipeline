b76553836665859a4c970ec2d8405ce270413d39
tbunetti <tonya.brunetti@colorado.edu>
2017-06-15 10:17:32 -0600

    fixed bug with usePCs parameter

--- export_var_slurm_streamlined.sh
+++ export_var_slurm_streamlined.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pathPLINKprefix=$1 rlib=$2 < test_script_mod_TB.sh sbatch --account=TICR-${USER}
+pathPLINKprefix=$1 rlib=$2 pcs=$3 < test_script_mod_TB.sh sbatch --account=TICR-${USER}
